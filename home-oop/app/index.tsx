659d7574f58ca253a170b8ef68cb13e40576a1a2
Christopher4113 <ch.lam1328@gmail.com>
2025-04-01 01:31:55 -0400

    got tailwind css in

--- home-oop/app/index.tsx
+++ home-oop/app/index.tsx
@@ -10,6 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className="font-bold text-lg my-10">Welcome to Home oop</Text>
       <Link href='/landing'>Landing</Link>
     </View>
   );
